393945c6a657da934ad273f89ed674c424d75f6d
red-panda-web <readex934@gmail.com>
2020-10-11 21:47:14 +0300

    added all blocks, menu, some adaptive, changing logo

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
+
 var mainSwiper = new Swiper('.swiper-container', {
 	mousewheel: {
 		invert: false,
 	},
 	speed: 5000,
 	freeMode: true,
-	//simulateTouch: false,
 });
+
+let open_menu = document.querySelector(".header__menu");
+//функция окрытия меню
+open_menu.addEventListener("click", function () {
+	document.querySelector(".menu").classList.toggle("active");	//открытие меню
+	document.querySelector(".header__logo").classList.toggle("hidd");	//скрытие логотипа
+	document.querySelector(".header__menu").classList.toggle("active");	//смена иконки меню
+	mainSwiper.mousewheel.disable();
+});
+
+
+setInterval(() => {
+	//блокировка скролла при открытом меню
+	if (!document.querySelector(".menu").classList.contains("active")) mainSwiper.mousewheel.enable();
+	//смена логотипа
+	if (mainSwiper.activeIndex >= 1) document.getElementById("logo").setAttribute("src", "img/icons/logo_green.png");
+	else document.getElementById("logo").setAttribute("src", "img/icons/logo_white.png");
+}, 500);
+
